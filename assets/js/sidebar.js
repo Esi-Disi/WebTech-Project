04ead53cbb2d509aef1fe16ae2f3918ae5b7c174
Ayush Kashyap <100943919+Esi-Disi@users.noreply.github.com>
2024-04-28 03:16:28 +0530

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -35,8 +35,8 @@ export function sidebar() {
 
       </div>
       <div class="sidebar-footer">
-        <p class="copyright">Copyright 2023</p>
-        <a href="https://github.com/shivam171" style="font-size:15px"><img src="https://img.icons8.com/ios-filled/25/ffffff/github.png" alt="github"/>shivam171</a>
+        <p class="copyright">Made by 22cd3006</p>
+        <a href="https://github.com/Esi-Disi" style="font-size:15px"><img src="https://img.icons8.com/ios-filled/25/ffffff/github.png" alt="github"/>Ayush Kashyap</a>
         <br/>
         <img
           src="./assets/images/tmdb-logo.png"
