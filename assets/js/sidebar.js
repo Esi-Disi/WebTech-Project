f7e540492a0d59d44df1c523a65e6aad1e66d658
Ayush Kashyap <100943919+Esi-Disi@users.noreply.github.com>
2024-04-28 03:18:56 +0530

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -30,8 +30,6 @@ export function sidebar() {
         <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=en", "English")'>English</a>
 
         <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=hi", "Hindi")'>Hindi</a>
-        
-        <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=bn", "Bengali")'>Bengali</a>
 
       </div>
       <div class="sidebar-footer">
@@ -40,7 +38,7 @@ export function sidebar() {
         <br/>
         <img
           src="./assets/images/tmdb-logo.png"
-          alt="the movie database logo"
+          alt="tmdb logo"
           width="130"
           height="17"
         />
